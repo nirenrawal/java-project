004dd212d71b72eff8d3fbd29c651023f0c22582
niren <nirenrawal@hotmail.com>
2021-01-21 22:42:41 +0100

    first commit

---
+++ src/main/java/com/nirendra/App.java
@@ -0,0 +1,13 @@
+package com.nirendra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
